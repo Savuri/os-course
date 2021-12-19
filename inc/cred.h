4ea323e32e76b6904ba3807f1d2c09ebd16242c5
Yuri Savinykh <yuras2002s@gmail.com>
2021-12-19 20:17:39 +0300

    Fix tabs

--- inc/cred.h
+++ inc/cred.h
@@ -6,17 +6,16 @@
 #define NGROUPS_MAX 32
 
 struct Cred {
-//	u_int	ref;			/* reference count */
+    //	u_int	ref;			/* reference count */
 
-	uid_t	uid;	     	        /* user id. */
-    uid_t	euid;			        /* effective user id */
-	uid_t	svuid;	            	/* Saved effective user id. */
-	gid_t	gid;		        	/* group id. */
-	gid_t	egid;    	        	/* effective group id */
-	gid_t	svgid;	            	/* Saved effective group id. */
-	short	ngroups; 	        	/* number of groups */
-	gid_t	groups[NGROUPS_MAX];	/* groups */
+    uid_t uid;                 /* user id. */
+    uid_t euid;                /* effective user id */
+    uid_t svuid;               /* Saved effective user id. */
+    gid_t gid;                 /* group id. */
+    gid_t egid;                /* effective group id */
+    gid_t svgid;               /* Saved effective group id. */
+    short ngroups;             /* number of groups */
+    gid_t groups[NGROUPS_MAX]; /* groups */
 };
 
 #endif /* !JOS_INC_UCRED_H */
-
