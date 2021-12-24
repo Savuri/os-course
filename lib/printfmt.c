fbd9831a7517cb656bf0881aaf3aeb2694f0fa10
DoricRedPanda <azatoth@ro.ru>
2021-12-24 12:21:21 +0300

    error code: fix style, sync with lib/libfmt.c

--- lib/printfmt.c
+++ lib/printfmt.c
@@ -38,6 +38,7 @@ static const char *const error_string[MAXERROR] = {
         [E_FILE_EXISTS] = "file already exists",
         [E_NOT_EXEC] = "file is not a valid executable",
         [E_NOT_SUPP] = "operation not supported",
+        [E_PERM] = "operation not permitted",
 };
 
 /*
