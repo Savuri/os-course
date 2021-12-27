43c527085b4211511f1f7cdb46451a1a4de4f9f0
Alexey <s02190176@gse.cs.msu.ru>
2021-12-27 11:48:09 +0300

    user.c: replace 1024 to NBUFSIZ

--- lib/user.c
+++ lib/user.c
@@ -2,7 +2,7 @@
 #include <user/user.h>
 #include <inc/string.h>
 
-static char user_file_buf[1024];
+static char user_file_buf[NBUFSIZ];
 
 static uid_t
 readuid() {
@@ -16,7 +16,7 @@ readuid() {
     for (; user_file_buf[i] != ':'; i++)
         ;
     user_file_buf[i] = 0;
-    return (uid_t)(strtol(user_file_buf+l-1, NULL, 10));
+    return (uid_t)(strtol(user_file_buf + l - 1, NULL, 10));
 }
 
 static gid_t
@@ -33,7 +33,7 @@ isuserexist(uid_t uid) {
     int fd = open("/etc/passwd", O_RDONLY);
     if (fd < 0)
         return 0;
-    while (getline(fd, user_file_buf, 1024) == 1) {
+    while (getline(fd, user_file_buf, NBUFSIZ) == 1) {
         if (uid == readuid())
             return 1;
     }
@@ -47,7 +47,7 @@ isgroupexist(gid_t gid) {
     int fd = open("/etc/group", O_RDONLY);
     if (fd < 0)
         return 0;
-    while (getline(fd, user_file_buf, 1024)) {
+    while (getline(fd, user_file_buf, NBUFSIZ)) {
         if (gid == readgid())
             return 1;
     }
