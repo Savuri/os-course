e64a5665a85aacd819666e093cc22513f00cee93
Alexey <s02190176@gse.cs.msu.ru>
2021-12-27 08:50:54 +0300

    chmod:fixed

--- user/chown.c
+++ user/chown.c
@@ -8,17 +8,44 @@ usage() {
     exit();
 }
 
+/*
+ * -1 no finds
+ * -2 more than one :
+ *  pos :
+ */
+int
+finddelim(char * arg) {
+    long int i = 0;
+    long int res;
+    while((arg[i] != ':') && (i < strlen(arg)))
+        i++;
+    if(arg[i] == ':'){
+        res = i;
+        while(i < strlen(arg)){
+            i++;
+            if(arg[i] == ':')
+                res = -2;
+        }
+        return res;
+    }
+    return -1;
+}
+
 void
 umain(int argc, char** argv) {
     if(argc != 3){
         usage();
         return;
     }
-    if(!isuserexist((uid_t)(strtol(argv[1], NULL, 10)))) {
-        printf("Invalid UID\n");
-        return;
-    }
+    int delim = finddelim(argv[1]);
+    if(delim < 0)
+        usage();
+    argv[delim] = 0;
     //path check?
-    if(chown(argv[2], (uid_t)(strtol(argv[1], NULL, 10))))
+    if(chown(argv[2], (uid_t)(strtol(argv[1], NULL, 10)))){
         printf("Bad chown\n");
+        exit();
+    }
+    if(chgrp(argv[2], (gid_t)(strtol(argv[1]+delim+1, NULL, 10))))
+        printf("Bad chgrp\n");
 }
