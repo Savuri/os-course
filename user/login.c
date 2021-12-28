1928a344811686a36f37f36aaf3dbd77e79e2505
DoricRedPanda <azatoth@ro.ru>
2021-12-28 15:40:45 +0300

    login: timing attack protection

--- user/login.c
+++ user/login.c
@@ -117,6 +117,18 @@ parse_shadow_entry(char *buf, shadow_t *shadow) {
 }
 
 int
+passcmp(char *foo, char *bar) {
+    int res = 0;
+    while (*bar != '\0') {
+        if (*foo != *bar)
+            res = -1;
+        foo++;
+        bar++;
+    }
+    return res;
+}
+
+int
 checkpassword(const char login[], const char password[], passwd_t *passwd) {
     shadow_t shadow;
     int f, res;
@@ -144,7 +156,7 @@ checkpassword(const char login[], const char password[], passwd_t *passwd) {
             if (res < 0)
                 return res;
             bintob64(base64, hash, strlen(hash));
-            res = strcmp(base64, shadow.hash) ? -4 : 1;
+            res = passcmp(base64, shadow.hash) ? -4 : 1;
             break;
         }
     }
